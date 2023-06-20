7efa5154b756efe0bd34e2207d1ca996ef9f4b1e
SHIVALEELA11 <111143328+SHIVALEELA11@users.noreply.github.com>
2023-06-20 15:18:36 -0400

    remove the last object in the ArrayList

--- Java_training-32/src/Day16/ArrayListRemove1.java
+++ Java_training-32/src/Day16/ArrayListRemove1.java
@@ -1,2 +1,30 @@
-package Day16;public class ArrayListRemove1 {
+package Day16;
+
+import java.util.ArrayList;
+
+public class ArrayListRemove1 {
+
+    public static void main(String[] args)
+    {
+        //create ArrayList
+
+        ArrayList<String> arrayListColors= new ArrayList<String>();
+
+        //add some elements
+
+        arrayListColors.add("Red");
+        arrayListColors.add("white");
+        arrayListColors.add("Black");
+        arrayListColors.add("Orange");
+
+        /*To remove last element of ArrayList
+     use remove method.*/
+
+        if( arrayListColors.size() > 0 )
+            arrayListColors.remove( arrayListColors.size() - 1 );
+
+        //print ArrayList
+        System.out.println(arrayListColors);
+
+    }
 }
