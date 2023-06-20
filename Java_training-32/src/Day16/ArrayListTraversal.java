aba4bc8e1d0e18697aeaeab1342d5569c07560a2
SHIVALEELA11 <111143328+SHIVALEELA11@users.noreply.github.com>
2023-06-20 15:30:30 -0400

    traverse all elements of the list

---
+++ Java_training-32/src/Day16/ArrayListTraversal.java
@@ -0,0 +1,46 @@
+package Day16;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListTraversal {
+    public static void main(String[] args) {
+        // Create an ArrayList of Strings
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("Java");
+        stringList.add("Programming");
+
+        // Traverse using a for loop
+        System.out.println("Using for loop:");
+        for (int i = 0; i < stringList.size(); i++) {
+            String element = stringList.get(i);
+            System.out.println(element);
+        }
+
+        // Traverse using an enhanced for loop
+        System.out.println("\nUsing enhanced for loop:");
+        for (String element : stringList) {
+            System.out.println(element);
+        }
+
+        // Traverse using an iterator
+        System.out.println("\nUsing iterator:");
+        Iterator<String> iterator = stringList.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
+
+        // Traverse using forEach
+        System.out.println("\nUsing forEach:");
+        stringList.forEach(System.out::println);
+
+        // Traverse using parallel stream
+        System.out.println("\nUsing parallel stream:");
+        stringList.parallelStream().forEach(System.out::println);
+    }
+}
+
